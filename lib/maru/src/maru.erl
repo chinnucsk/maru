ca5f865fb012a51e911f6e2eb7901f23d2baf887
Tristan Sloughter <tristan.sloughter@gmail.com>
2012-03-03 15:37:01 -0600

    added sys config

--- lib/maru/src/maru.erl
+++ lib/maru/src/maru.erl
@@ -34,16 +34,21 @@ create_model(AppPrefix, ModelName) ->
 
 create_project(ProjectName, AppPrefix) ->
     create_dirs(project, ProjectName, AppPrefix),
-    create_config(ProjectName, AppPrefix),
+    create_build_config(ProjectName, AppPrefix),
+    create_sys_config(ProjectName),
     create_app_files(ProjectName, AppPrefix),
     create_source_files(ProjectName, AppPrefix),
     create_start_script(ProjectName, AppPrefix).
 
-create_config(ProjectName, AppPrefix) ->
+create_build_config(ProjectName, AppPrefix) ->
     maru_utils:write_template(build_config, [{project_name, ProjectName},
                                              {project_prefix, AppPrefix},
                                              {project_version, "0.0.1"}],
                               filename:join(ProjectName, "sinan.config")).
+
+create_sys_config(ProjectName) ->
+    maru_utils:write_template(sysconfig, [{project_name, ProjectName}],
+                              filename:join([ProjectName, "config", "sys.config"])).    
 
 create_app_files(ProjectName, AppPrefix) ->
     [create_app_file(Type, ProjectName, AppPrefix) ||
